f63fc4bf3c52ed2482561d4c7bcd21ac5661ca06
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-16 18:04:50 -0500

    prog_est_parcial_prog7
    Primer uso de fgets y la librería string.h

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # prog_est_parcial2_prog7
-Uso de la librería string.h
+<p><b>Autor:</b> Andriks Imanol Ruiz Martínez, <b>Fecha:</b> 31/03/2022 <br>
+  <b>Escuela:</b> Universidad del Valle de México <b>Campus</b> Villahermosa <br>
+  <b>Materia:</b> Programación Estructurada <br>
+  <b>Ciclo:</b> 01/2022</p>
+<br>
+<p>Este es un programa en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables enteras y en cadena</li>
+    <li>printf para mostrar mensajes y variables</li>
+    <li>fgets</li>
+    <li>El uso de include para las librerías</li>
+    <li>Librerías de la función string.h</li>
+    <li>Uso de condicionales</li>
+    <li>Operadores lógicos</li>
+    <li>Comentarios para la documentación interna del programa</li>
+    </ul>
+    </p>
+<br>
+<b>Objetivo:</b>Usar la librería string para cadenas de carácteres.
+<br>
+<br>
+<p><b>Breve descripción: </b>
+Se trata de un programa que utilice la librería string (string.h) para usar cadenas de carácteres y sus funciones más comunes. Se usa funciones como strlen y se aprende el uso de fgets.
+<br>
+</p>
---
+++ prog2_7.cpp
@@ -0,0 +1,41 @@
+/* Autor: Andriks Imanol Ruiz Mártinez, Realizado: 31/03/2022
+Escuela: Universidad del Valle de México Campus Villahermosa 
+Materia: Programación Estructurada
+Ciclo: 01/2022
+
+Este es un programa en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+ 
+    -Variables enteras y en cadena
+    -printf para mostrar mensajes y variables
+    -fgets
+    -El uso de include para las librerías
+    -Librerías de la función string.h
+    -Uso de condicionales
+    -Comentarios para la documentación interna del programa
+*/
+#include<stdio.h>
+#include<string.h>
+int main(){
+	//Declaración de cadenas
+	char nombre[]="Andriks Imanol Ruiz Martinez";
+	char *carrera="Ing. en Sistemas Computacionales";
+	char nombre2[30];
+	int longitud;
+	//Entrada por teclado de cadenas con scanf no lee espacios
+	printf("Introduce el nombre: ");
+//	scanf("%s", &nombre2);
+//Usando gets, funciona, lee todo, pero no verifica el tamaño de la cadena
+//gets(nombre2);
+	fgets(nombre2,30,stdin); //Lee bien, y solamente lo que cabe en la cadena
+	printf("Nombre2: [%s]\n", nombre2);
+	printf("Nombre: [%s]\n", nombre);
+	printf("Carrera: [%s]\n", carrera);
+	printf("La longitud de nombre 2 es %d\n", longitud);
+	if (strlen(nombre)>strlen(nombre2)){
+		printf("Nombre tiene mas caracteres\n");
+	}
+	strcpy(nombre2, "Hola mundo!!!");
+	printf("Nombre2= [%s]\n", nombre2);
+	return 0;
+}
